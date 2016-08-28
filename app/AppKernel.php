04cfa389ed147f2cdcdb0a915ec8bdd0b5a0d37c
FX Soubirou <soubirou@yahoo.fr>
2016-08-28 15:23:49 +0200

    Twitter bootstrap UI with flatly theme - Fix #15

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new AppBundle\AppBundle(),
         ];
 
